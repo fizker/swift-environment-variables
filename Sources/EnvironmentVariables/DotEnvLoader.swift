7cbf7dc86bcf594e2db24b410b76d572b9ae3547
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-05-24 09:36:02 +0200

    Added first test for loading env-files directly

--- Sources/EnvironmentVariables/DotEnvLoader.swift
+++ Sources/EnvironmentVariables/DotEnvLoader.swift
@@ -1,6 +1,107 @@
+import Foundation
+
 typealias EnvFile = [String: String]
 
 public struct DotEnvLoader {
+	/// The location that the Loader should look for `.env` files
+	public enum Location {
+		/// The currently configured working directory.
+		case currentWorkingDir
+		/// The directory that the executable is located in.
+		case executableDir
+		/// A custom path.
+		///
+		/// - If this is the path to a file, it will be treated as a `.env` file regardless of the name.
+		/// - If this is the path of a directory, `.env` will be appended to the path.
+		case path(String)
+	}
+
+	private let fm: FileManager
+	private let pi: ProcessInfo
+	private var files: [EnvFile] = []
+
+	public init(locations: [Location] = [.executableDir, .currentWorkingDir], fileManager: FileManager = .default, processInfo: ProcessInfo = .processInfo) {
+		fm = fileManager
+		pi = processInfo
+
+		var hasFoundCWD = false
+		var hasFoundExecutableDir = false
+
+		for location in locations {
+			let path: String
+			switch location {
+			case let .path(p):
+				path = p
+			case .currentWorkingDir:
+				guard !hasFoundCWD
+				else { continue }
+				hasFoundCWD = true
+				var url = URL(fileURLWithPath: fm.currentDirectoryPath)
+				url.appendPathComponent(".env")
+				path = url.path
+			case .executableDir:
+				guard !hasFoundExecutableDir
+				else { continue }
+				hasFoundExecutableDir = true
+
+				let executablePath = pi.arguments[0]
+				if #available(macOS 13.0, *) {
+					var dir = URL(filePath: executablePath)
+					dir.deleteLastPathComponent()
+					dir.append(component: ".env")
+					path = dir.path()
+				} else {
+					var dir = URL(fileURLWithPath: executablePath)
+					dir.deleteLastPathComponent()
+					dir.appendPathComponent(".env")
+					path = dir.path
+				}
+			}
+
+			if let file = handle(path: path) {
+				files.append(file)
+			}
+		}
+	}
+
+	func handle(path: String) -> EnvFile? {
+		var isDir: ObjCBool = false
+		guard fm.fileExists(atPath: path, isDirectory: &isDir)
+		else { return nil }
+
+		let data: Data?
+		if #available(macOS 13.0, *) {
+			var url = URL(filePath: path)
+			if isDir.boolValue {
+				url.append(component: ".env")
+			}
+			data = fm.contents(atPath: url.path())
+		} else {
+			var url = URL(fileURLWithPath: path)
+			if isDir.boolValue {
+				url.appendPathComponent(".env")
+			}
+			data = fm.contents(atPath: url.path)
+		}
+
+		guard
+			let data,
+			let content = String(data: data, encoding: .utf8)
+		else { return nil }
+
+		return Self.parse(file: content)
+	}
+
+	public func get(_ key: String) -> String? {
+		for file in files {
+			if let value = file[key] {
+				return value
+			}
+		}
+
+		return nil
+	}
+
 	static func parse(file: String) -> EnvFile? {
 		enum State {
 			case key, value, comment
--- Tests/EnvironmentVariablesTests/DotEnvLoaderTests.swift
+++ Tests/EnvironmentVariablesTests/DotEnvLoaderTests.swift
@@ -207,4 +207,60 @@ final class DotEnvLoaderTests: XCTestCase {
 			"bar": "1\r\n2",
 		])
 	}
+
+	func test__initWithLocations__specificFile__initiatesCorrectly() async throws {
+		let fm = FileManager.default
+
+		guard
+			let path = Bundle.module.path(forResource: "sample-env", ofType: nil),
+			let data = fm.contents(atPath: path)
+		else {
+			XCTFail("Failed to load file")
+			return
+		}
+
+		let subject = DotEnvLoader(locations: [.path(path)])
+
+		let expected = [
+			"BASIC": "basic",
+			"AFTER_LINE": "after_line",
+			"EMPTY": "",
+			"EMPTY_SINGLE_QUOTES": "",
+			"EMPTY_DOUBLE_QUOTES": "",
+			"EMPTY_BACKTICKS": "",
+			"SINGLE_QUOTES": "single_quotes",
+			"SINGLE_QUOTES_SPACED": "    single quotes    ",
+			"DOUBLE_QUOTES": "double_quotes",
+			"DOUBLE_QUOTES_SPACED": "    double quotes    ",
+			"DOUBLE_QUOTES_INSIDE_SINGLE": #"double "quotes" work inside single quotes"#,
+			"DOUBLE_QUOTES_WITH_NO_SPACE_BRACKET": "{ port: $MONGOLAB_PORT}",
+			"SINGLE_QUOTES_INSIDE_DOUBLE": "single 'quotes' work inside double quotes",
+			"BACKTICKS_INSIDE_SINGLE": "`backticks` work inside single quotes",
+			"BACKTICKS_INSIDE_DOUBLE": "`backticks` work inside double quotes",
+			"BACKTICKS": "backticks",
+			"BACKTICKS_SPACED": "    backticks    ",
+			"DOUBLE_QUOTES_INSIDE_BACKTICKS": #"double "quotes" work inside backticks"#,
+			"SINGLE_QUOTES_INSIDE_BACKTICKS": #"single 'quotes' work inside backticks"#,
+			"DOUBLE_AND_SINGLE_QUOTES_INSIDE_BACKTICKS": #"double "quotes" and single 'quotes' work inside backticks"#,
+			"EXPAND_NEWLINES": "expand\nnew\nlines",
+			"DONT_EXPAND_UNQUOTED": "dontexpand\\nnewlines",
+			"DONT_EXPAND_SQUOTED": "dontexpand\\nnewlines",
+			"INLINE_COMMENTS": "inline comments",
+			"INLINE_COMMENTS_SINGLE_QUOTES": "inline comments outside of #singlequotes",
+			"INLINE_COMMENTS_DOUBLE_QUOTES": "inline comments outside of #doublequotes",
+			"INLINE_COMMENTS_BACKTICKS": "inline comments outside of #backticks",
+			"INLINE_COMMENTS_SPACE": "inline comments start with a",
+			"EQUAL_SIGNS": "equals==",
+			"RETAIN_INNER_QUOTES": #"{"foo": "bar"}"#,
+			"RETAIN_INNER_QUOTES_AS_STRING": #"{"foo": "bar"}"#,
+			"RETAIN_INNER_QUOTES_AS_BACKTICKS": #"{"foo": "bar's"}"#,
+			"TRIM_SPACE_FROM_UNQUOTED": "some spaced out string",
+			"USERNAME": "therealnerdybeast@example.tld",
+			"SPACED_KEY": "parsed",
+		]
+
+		for (key, value) in expected {
+			XCTAssertEqual(subject.get(key), value)
+		}
+	}
 }
