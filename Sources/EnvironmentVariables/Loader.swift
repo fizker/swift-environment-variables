08d45d11fd1cefedfe0d5f25dee61e90940d6b59
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-07-27 19:25:44 +0200

    Added a designated shorthand for getting values from the environment

--- Sources/EnvironmentVariables/Loader.swift
+++ Sources/EnvironmentVariables/Loader.swift
@@ -13,15 +13,20 @@ extension Loader where Self == MultiLoader {
 	/// A default loader that looks in the environment for the process, as well as looks for `.env` files in the current working directory for the process and the folder where the executable is.
 	///
 	/// The priority is as follows:
-	/// 1. The environment
+	/// 1. The ``environment`` for the current process.
 	/// 2. The current working directory.
 	/// 3. The directory containing the executable.
 	public static var `default`: MultiLoader {
 		MultiLoader(loaders: [
-			ProcessInfo.processInfo.environment,
+			.environment,
 			DotEnvLoader(location: .currentWorkingDir),
 			DotEnvLoader(location: .executableDir),
 		])
+	}
+
+	/// A default loader that looks for values in the environment for the current process.
+	public static var environment: some Loader {
+		ProcessInfo.processInfo.environment
 	}
 }
 
