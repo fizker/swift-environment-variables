234d44749836cac1b8e658710eceb994a2e2380f
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-09-18 09:16:52 +0200

    Converted one file to Testing framework

--- Tests/EnvironmentVariablesTests/MissingEnvironmentVariablesTests.swift
+++ Tests/EnvironmentVariablesTests/MissingEnvironmentVariablesTests.swift
@@ -1,14 +1,16 @@
-import XCTest
+import Testing
 @testable import EnvironmentVariables
 
-final class MissingEnvironmentVariablesTests: XCTestCase {
-	func test__description__singleErrorProvided__returnsCorrectDescription() async throws {
+final class MissingEnvironmentVariablesTests {
+	@Test
+	func description__singleErrorProvided__returnsCorrectDescription() async throws {
 		let subject = MissingEnvironmentVariables(keys: ["b"])
-		XCTAssertEqual(subject.description, "Following required env keys are missing: b")
+		#expect(subject.description == "Following required env keys are missing: b")
 	}
 
-	func test__description__multipleErrorsProvided__returnsCorrectDescription() async throws {
+	@Test
+	func description__multipleErrorsProvided__returnsCorrectDescription() async throws {
 		let subject = MissingEnvironmentVariables(keys: ["b", "a", "c"])
-		XCTAssertEqual(subject.description, "Following required env keys are missing: b, a, c")
+		#expect(subject.description == "Following required env keys are missing: b, a, c")
 	}
 }
